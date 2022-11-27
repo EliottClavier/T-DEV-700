625bd88e17258fcbe34be3b355e37c1dc8bf6f7f
Xamimus <pro.maximemartin@gmail.com>
2022-11-27 16:19:34 +0100

    [ADD] Add payment success screen integration

--- tpe/lib/main.dart
+++ tpe/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:tpe/screens/home.dart';
 import 'package:tpe/screens/payment.dart';
+import 'package:tpe/screens/payment_success.dart';
 
 //home
 //void main() => runApp(const PaymentTerminalApp());
 
 //Payment
-void main() => runApp(const PaymentScreen());
+//void main() => runApp(const PaymentScreen());
+
+//Payment success
+void main() => runApp(const PaymentSuccessScreen());
---
+++ tpe/lib/screens/payment_success.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class PaymentSuccessScreen extends StatelessWidget {
+  const PaymentSuccessScreen({super.key});
+
+  static const String _title = 'Payment method';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: const PaymentSuccessScreenStatefulWidget(),
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFF1EBE4B),
+          fontFamily: "Montserrat"),
+    );
+  }
+}
+
+class PaymentSuccessScreenStatefulWidget extends StatefulWidget {
+  const PaymentSuccessScreenStatefulWidget({super.key});
+
+  @override
+  State<PaymentSuccessScreenStatefulWidget> createState() =>
+      _PaymentSuccessScreenStatefulWidgetState();
+}
+
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _PaymentSuccessScreenStatefulWidgetState
+    extends State<PaymentSuccessScreenStatefulWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onBackHome() {
+    print("Back home");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Image(image: AssetImage("assets/img/check-circle.png")),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      "Paiement réalisé avec succès",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                        fontSize: 15,
+                        letterSpacing: 0.02,
+                        height: 1.2,
+                      ),
+                    ),
+                    Text(
+                      "Votre paiement de 0.00 € a bien été réalisé",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 15,
+                        letterSpacing: 0.02,
+                        height: 1.2,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                OutlinedButton(
+                    onPressed: _onBackHome,
+                    style: ButtonStyle(
+                      minimumSize:
+                          MaterialStateProperty.all(const Size(250, 50)),
+                    ),
+                    child: const Text(
+                      "Revenir à l'accueil",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                        fontSize: 15,
+                        height: 1.2,
+                      ),
+                    ))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
