768c3e0609a79ba05fb40557e57680c636590908
paulripault <paulripaut0@gmail.com>
2022-12-05 15:08:20 +0100

    [ADD] qrcode-generation-api

---
+++ api/src/main/java/com/api/tpe/controller/QrcodeController.java
@@ -0,0 +1,92 @@
+package com.api.tpe.controller;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class QrcodeController {
+    private static SecretKeySpec secretKey;
+
+
+    public qrCodeModel (int id, float amount) throws Exception {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public static void main(String[] args) throws IOException, WriterException {
+        createQrcode(45, 2, "maison");
+    }
+    public static void createQrcode(float amount, int id, String key) throws IOException, WriterException {
+        String cash = String.valueOf(amount);
+        String intId = String.valueOf(id);
+        String hash = cash + ":" + intId;
+        String encryptedString = QrcodeController.encrypt(hash, key);
+
+        generateQRcode(encryptedString);
+    }
+    public static void setKey(final String myKey) {
+        MessageDigest sha;
+        try {
+            byte[] key = myKey.getBytes(StandardCharsets.UTF_8);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String encrypt(final String strToEncrypt, final String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e);
+        }
+        return null;
+    }
+    public static String decrypt(final String strToDecrypt, final String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder()
+                    .decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e);
+        }
+        return null;
+    }
+
+    public static void QRcode(String data, String path, String charset, int h, int w) throws WriterException, IOException
+    {
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, w, h);
+        MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+    }
+
+    public static void generateQRcode(String check) throws WriterException, IOException
+    {
+        String path = "C:\\Users\\paulr\\Desktop\\test.png";
+        String charset = "UTF-8";
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<>();
+        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        QRcode(check, path, charset, 150, 150);
+    }
+}
