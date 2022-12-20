2de3b346e4c957eaad319f021dd0d5ba2dd10c41
Xamimus <pro.maximemartin@gmail.com>
2022-12-20 21:19:09 +0100

    [FIX] Override env variables in local with command line args

--- tpe/lib/config/environment/local/config.dart
+++ tpe/lib/config/environment/local/config.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
-const String LOCAL_API_URL = "192.168.1.11:8080/api";
+const String LOCAL_API_URL =
+    String.fromEnvironment('API_URL', defaultValue: '192.168.1.11:8080/api');
 const String LOCAL_REGISTER_HEADER = String.fromEnvironment(
     'TPE_REGISTER_SECRET_HEADER',
-    defaultValue: 'secretHeader');
-const String LOCAL_REGISTER_KEY = String.fromEnvironment(
-    'TPE_REGISTER_SECRET_KEY',
-    defaultValue: 'secretKey');
+    defaultValue: 'Authorization-TPE-Register');
+const String LOCAL_REGISTER_KEY =
+    String.fromEnvironment('TPE_REGISTER_SECRET_KEY', defaultValue: '');
