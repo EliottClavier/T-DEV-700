deb42b521b51113f669bc5c068b594b009cec701
paulripault <paulripaut0@gmail.com>
2022-12-27 19:05:24 +0100

    [FIX] old env structure

--- api/src/main/java/com/api/admin/auth/controller/AuthAdminController.java
+++ api/src/main/java/com/api/admin/auth/controller/AuthAdminController.java
@@ -1,21 +1,15 @@
 package com.api.admin.auth.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/auth")
 public class AuthAdminController {
 
-    @GetMapping(value = "/admin/auth/login/")
-    public String getTemplate(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "thymeleafTemplate";
-    }
-
-    @GetMapping(value = "/admin/whitelist/")
-    public String whitelist() {
-        return "thymeleafTemplate";
+    @GetMapping( value = {"/login/", "/login"})
+    public String getTemplate() {
+        return "loginView";
     }
 }
