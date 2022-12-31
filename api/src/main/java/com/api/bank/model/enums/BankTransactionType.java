aba3a6af870c52f00c2bdd0eef4cad54bc140dc8
Clemathe <clement.mathe@rdtingenieurs.fr>
2022-12-31 13:24:36 +0100

    [add] Enum Comment

--- api/src/main/java/com/api/bank/model/enums/BankTransactionType.java
+++ api/src/main/java/com/api/bank/model/enums/BankTransactionType.java
@@ -1,5 +1,8 @@
 package com.api.bank.model.enums;
 
+/**
+ * Type of transaction. (About shopping, QrCheck, etc.)
+ */
 public enum BankTransactionType {
     QR_CHECK,
     SHOPPING
--- api/src/main/java/com/api/bank/model/enums/PaymentMethod.java
+++ api/src/main/java/com/api/bank/model/enums/PaymentMethod.java
@@ -1,5 +1,6 @@
 package com.api.bank.model.enums;
 
+
 public enum PaymentMethod {
     CARD,
     CHECK,
