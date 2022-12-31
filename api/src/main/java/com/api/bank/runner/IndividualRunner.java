9202e5b41612cd9137199ead6fdd238d267394c6
Clemathe <clement.mathe@rdtingenieurs.fr>
2022-12-31 15:17:42 +0100

    [add] IndividualRunner

--- api/src/main/java/com/api/bank/runner/IndividualRunner.java
+++ api/src/main/java/com/api/bank/runner/IndividualRunner.java
@@ -1,8 +1,8 @@
 package com.api.bank.runner;
 
 import com.api.bank.model.entity.Account;
+import com.api.bank.model.entity.Card;
 import com.api.bank.model.entity.Client;
-import com.api.bank.model.entity.Shop;
 import com.api.bank.model.enums.SocialReasonStatus;
 import com.api.bank.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,24 +12,28 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-import static com.api.bank.model.BankConstants.BANK_NAME;
-
+/**
+ * This class is used to create a default Individual account
+ */
 @Component
-public class BankRunner implements ApplicationRunner {
-
+public class IndividualRunner implements ApplicationRunner {
 
     private final AccountService accountService;
 
     @Autowired
-    public BankRunner(AccountService accountService) {
+    public IndividualRunner(AccountService accountService) {
         this.accountService = accountService;
     }
+
     @Override
     public void run(ApplicationArguments args) {
-        Account accountSearch = accountService.getAccountByOwnerName(BANK_NAME);
+        UUID id = UUID.fromString("e45f2bc3-1ef0-4ed9-94b3-146483c66bea");
+        Account accountSearch = accountService.getAccountByClientId(id);
         if (accountSearch == null) {
-            Client client = new Client(UUID.randomUUID(), BANK_NAME, SocialReasonStatus.BANK);
-            Account account = new Account(100000000, client);
+            Client client = new Client(id, "Ivain", "Lamarche", SocialReasonStatus.INDIVIDUAL);
+            Card card = new Card("1234567890123456"); //TODO get the real card number
+            Account account = new Account(5000, client, card);
+
             accountService.add(account);
         }
     }
