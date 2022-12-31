9f7ecdc65534b2c8bc950c357e2358389954e938
Clemathe <clement.mathe@rdtingenieurs.fr>
2022-12-31 16:05:56 +0100

    [fix] Running test context

--- api/src/test/java/com/api/bank/ApiApplicationTests.java
+++ api/src/test/java/com/api/bank/ApiApplicationTests.java
@@ -1,17 +1,28 @@
 package com.api.bank;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-//@EnableJpaRepositories(value = {"com.api.bank.repository", "com.api.auth.repository", "com.api.gateway.tpe.repository"})
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.config.location=classpath:application.yml"},  args = {"--spring.main.allow-bean-definition-overriding=true"})
+
+@SpringBootTest(classes = {ApiApplicationTests.class})
+@ExtendWith(SpringExtension.class)
 class ApiApplicationTests {
 
     @Test
     void contextLoads() {
+       var a= 1;
+        assertEquals(1, a);
     }
 
 }
